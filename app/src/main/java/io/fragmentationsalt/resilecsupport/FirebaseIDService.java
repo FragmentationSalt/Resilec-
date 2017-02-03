d8dc6012e14bb146dc9a2566980d386d2a5079d3
786kaizersoze <Rrm78610>
2017-02-03 20:13:54 +0530

    php commit

---
+++ app/src/main/java/io/fragmentationsalt/resilecsupport/FirebaseIDService.java
@@ -0,0 +1,51 @@
+package io.fragmentationsalt.resilecsupport;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+/**
+ * Created by rohan on 2/1/2017.
+ */
+
+public class FirebaseIDService extends AsyncTask<Void, Void, String>  {
+
+    private String token;
+        FirebaseIDService(String token) {
+            this.token = token;
+        }
+    @Override
+    protected String doInBackground(Void... params) {
+        try
+        {
+            String response = null;
+            String tokenurl = "http://fragsalt.pe.hu/register.php";
+            String data = URLEncoder.encode("Token","UTF-8") + "=" + URLEncoder.encode(token,"UTF-8");
+            URL url = new URL(tokenurl);
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            while((response  = reader.readLine())!= null)
+            {
+                sb.append(response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
--- app/src/main/java/io/fragmentationsalt/resilecsupport/MainActivity.java
+++ app/src/main/java/io/fragmentationsalt/resilecsupport/MainActivity.java
@@ -1,48 +1,32 @@
 package io.fragmentationsalt.resilecsupport;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.test.mock.MockPackageManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import com.github.nkzawa.emitter.Emitter;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.URISyntaxException;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MainActivity extends AppCompatActivity{
-
-
-    //Code to request for location
-    //User Defined as 2
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
     // GPSTracker class
     GPSTracker gps;
 
-    public Socket mSocket;
     double latitude;
     double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            mSocket = IO.socket("http://ec569933.ngrok.io");
-            mSocket.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
         setContentView(R.layout.activity_main);
-        try {
+        startService(new Intent(getApplicationContext(),FirebaseInstanceIDService.class));
+           try {
             if (ActivityCompat.checkSelfPermission(this, mPermission)
                     != MockPackageManager.PERMISSION_GRANTED) {
 
@@ -52,61 +36,28 @@ public class MainActivity extends AppCompatActivity{
                 // If any permission above not allowed by user, this condition will
                 //execute every time, else your else part will work
             }
+            gps = new GPSTracker(MainActivity.this);
+
+            // check if GPS enabled
+            if(gps.canGetLocation()){
+
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+
+                // \n is for new line
+                Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
+                        + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+            }else{
+                // can't get location
+                // GPS or Network is not enabled
+                // Ask user to enable GPS/network in settings
+                gps.showSettingsAlert();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-
-                    mSocket.on("global test", onNewMessage);
-                    gps = new GPSTracker(MainActivity.this);
-
-                    // check if GPS enabled
-                    if(gps.canGetLocation()){
-
-                        latitude = gps.getLatitude();
-                        longitude = gps.getLongitude();
-
-                        // \n is for new line
-                        Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
-                                + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                    }else{
-                        // can't get location
-                        // GPS or Network is not enabled
-                        // Ask user to enable GPS/network in settings
-                        gps.showSettingsAlert();
-                    }
-                    mSocket.emit("new message", String.valueOf(latitude)+","+String.valueOf(longitude));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            JSONObject data = (JSONObject) args[0];
-            //String username;
-            try {
-                Log.d("TAG",data.getString("str"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            try {
-                System.out.print(data.getString("str"));
-                Log.d("TAG","Got Here");
-            } catch (JSONException e) {
-                Log.d("TAG","Here");
-            }
-            // add the message to view
-            //addMessage(username, message);
-        }
 
-    };
 
 }
 
